bf6321d39a9b9d74254f223de51a7eb0c00d7d44
Rafael Galindo <rgf1996cbta3@gmail.com>
2025-06-15 01:46:11 -0600

    Miercoles de cambios

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-/*!
-
-=========================================================
-* Paper Dashboard React - v1.3.1
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom"; // Cambiado: No usamos '/client'
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -27,13 +9,13 @@ import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 import AdminLayout from "layouts/Admin.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
+// Cambiado: Usamos ReactDOM.render en lugar de createRoot
+ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
       <Redirect to="/admin/dashboard" />
     </Switch>
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root") // Segundo argumento para React 17
 );
